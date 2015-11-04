05ef88a82f94865a4f54314655d910d8cd0325bf
Dave Sullivan <dbsullivan@madisoncollege.edu>
2015-11-04 09:39:15 -0600

    update working final version homework! 20151104

--- src/edu/madisoncollege/enterprisejava/EmployeeDriver.java
+++ src/edu/madisoncollege/enterprisejava/EmployeeDriver.java
@@ -20,23 +20,31 @@ public class EmployeeDriver {
         EmployeeDao dao = new EmployeeDao();
         try {
             Database.getInstance().connect();
+            // 1. getAllEmployees
             System.out.println(dao.getAllEmployees());
 
             //TODO add code to call the other dao methods
-            System.out.println(dao.getEmployee(109));
+            // 2. getEmployee
+            System.out.println("Employee 109: " + dao.getEmployee(109));
 
+            // 3. updateEmployee
+            int employeeId = 200;
+            String firstName = "Sammy";
+            String lastName = "Davis";
+            String socialSecurityNumber = "222-33-4444";
+            String department = "ET";
+            String room = "100";
+            String phone = "4148887771";
 
-    // update employee 116 with new last name
-//            System.out.println("Before update: " + dao.getEmployee(116));
-//
-//            int employeeId = 116;
-//            String lastName = "Tillman-Spillman";
-//            Employee UpdateEmployee = dao.getEmployee(116);
-//            dao.updateEmployee(UpdateEmployee);
-//            System.out.println("After update: " + dao.getEmployee(116));
+            Employee AddEmployee = new Employee(employeeId, firstName, lastName, socialSecurityNumber, department, room, phone);
+            dao.updateEmployee(AddEmployee);
 
+            System.out.println("Employee 200: " + dao.getEmployee(200));
 
-//           deleteStudent(Employee employee)
+          // 4. deleteEmployee
+            Employee deleteEmployee = new Employee(200, null, null, null, null, null, null);
+            dao.deleteEmployee(deleteEmployee);
+//            System.out.println(dao.getEmployee(200)); // java.lang.IndexOutOfBoundsException: Index: 0, Size: 0 if he's gone.
 
         } catch (SQLException e) {
             e.printStackTrace();
--- src/edu/madisoncollege/enterprisejava/persistence/EmployeeDao.java
+++ src/edu/madisoncollege/enterprisejava/persistence/EmployeeDao.java
@@ -46,12 +46,13 @@ public class EmployeeDao {
 
     public Employee getEmployee(int employeeId) throws SQLException {
         // TODO implement this method
-//        return null;
+
             List<Employee> employees = new ArrayList<Employee>();
 
             Connection connection = Database.getInstance().getConnection();
 
             String sql = "select * from employees where emp_id=" + employeeId;
+
             Statement selectStatement = connection.createStatement();
 
             ResultSet results = selectStatement.executeQuery(sql);
@@ -73,40 +74,36 @@ public class EmployeeDao {
             selectStatement.close();
 
             return employees.get(0);
-
         }
 
     public void updateEmployee(Employee employee)  throws SQLException  {
         // TODO implement this method
-//        Connection connection = Database.getInstance().getConnection();
-//
-//        // use all getters and setters, from UpdateEmployee on existing getEmployee object.
-//        String sql = "update employees set lastName=" + getEmployee(employee.);
-//        Statement selectStatement = connection.createStatement();
-//
-//        ResultSet results = selectStatement.executeQuery(sql);
-//
-//        while (results.next()) {
-//            int employeeId = results.getInt("emp_id");
-//            String firstName = results.getString("first_name");
-//            String lastName = results.getString("last_name");
-//            String socialSecurityNumber = results.getString("ssn");
-//            String department = results.getString("dept");
-//            String room = results.getString("room");
-//            String phone = results.getString("phone");
-//
-//            Employee employee = new Employee(employeeId, firstName, lastName, socialSecurityNumber, department, room, phone);
-//            employees.add(employee);
-//        }
-//
-//        results.close();
-//        selectStatement.close();
+        Connection connection = Database.getInstance().getConnection();
 
+        String sql = "insert into employees (emp_id,first_name,last_name,ssn,dept,room,phone) values ( " +
+                employee.getEmployeeId() + "," +
+               "'" + employee.getFirstName() + "', " +
+               "'" +  employee.getLastName() + "', " +
+               "'" +  employee.getSocialSecurityNumber() + "', " +
+               "'" +   employee.getDepartment() + "', " +
+               "'" +  employee.getRoom() + "', " +
+               "'" +  employee.getPhone() + "'" +
+               " )";
+
+        Statement updateStatement = connection.createStatement();
+        updateStatement.executeUpdate(sql);
+        updateStatement.close();
     }
 
 
-    public void deleteStudent(Employee employee) {
+    public void deleteEmployee(Employee employee) throws SQLException {
         // TODO implement this method
+        Connection connection = Database.getInstance().getConnection();
 
+        String sql = "delete from employees where emp_id=" + employee.getEmployeeId();
+
+        Statement deleteStatement = connection.createStatement();
+        deleteStatement.executeUpdate(sql);
+        deleteStatement.close();
     }
 }
